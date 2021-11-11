42c7c2c16bebcd0e7bccda98969ba85b6bfe3997
Payton  Provo <paytonprovo1@gmail.com>
2021-11-11 14:45:32 -0500

    added map and list

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,19 +1,33 @@
 import React from 'react';
-// import GoogleMapReact from 'google-map-react';
-// import { Paper, Typography, useMediaQuery } from '@material-ui/core';
-// import LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined';
-// import Rating from '@material-ui/lab/Rating';
+import GoogleMapReact from 'google-map-react';
+import { Paper, Typography, useMediaQuery } from '@material-ui/core';
+import LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined';
+import Rating from '@material-ui/lab/Rating';
 
 // import mapStyles from '../../mapStyles';
-// import useStyles from './styles.js';
+import useStyles from './styles.js';
 
 const Map = () => {
-    // const classes = useStyles();
+    const classes = useStyles();
+    const isMobile = useMediaQuery('(min-width:600px)');
+
+    const coordinates = { lat: 0, lng: 0 };
 
     return (
-        <h1>
-           Map 
-        </h1>
+        <div className={classes.mapContainer}>
+            <GoogleMapReact
+                bootstrapURLKeys={{ key: 'AIzaSyCjs39r9UXYmCOgn9ibYE8EwS_nmG_Mst8' }}
+                defaultCenter={coordinates}
+                center={coordinates}
+                defaultZoom={14}
+                margin={[50, 50, 50, 50]}
+                // options={''}
+                // onChange={''}
+                // onChildClick={''}
+            >
+
+            </GoogleMapReact>
+        </div>
     );
 };
 
