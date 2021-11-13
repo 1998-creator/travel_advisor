be01cf2a6b31a059e089452754294dd9c2f6b2a5
Payton  Provo <paytonprovo1@gmail.com>
2021-11-13 01:34:33 -0500

    added restaurant card

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -4,22 +4,11 @@ import { CircularProgress, Grid, Typography, InputLabel, MenuItem, FormControl, 
 import useStyles from './styles.js';
 import PlaceDetails from '../PlaceDetails/PlaceDetails';
 
-const List = () => {
+const List = ({ places }) => {
     const classes = useStyles();
     const [type, setType] = useState('restaurants');
     const [rating, setRating] = useState('');
 
-    const places = [
-        { name: 'Cool Place'},
-        { name: 'Best Beer'},
-        { name: 'Best Steak'},
-        { name: 'Cool Place'},
-        { name: 'Best Beer'},
-        { name: 'Best Steak'},
-        { name: 'Cool Place'},
-        { name: 'Best Beer'},
-        { name: 'Best Steak'}
-    ];
 
     return (
         <div className={classes.container}>
